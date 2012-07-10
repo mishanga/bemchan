bf434ee94a0b0c9b6a4fe8e451d613a5cfc75c8e
Mikhail Troshev <mishanga@yandex-team.ru>
2012-07-10 20:56:04 +0000

    Simple example server script

---
+++ server.js
@@ -0,0 +1,15 @@
+var http = require('http'),
+    url = require('url'),
+    fs = require('fs');
+
+http.createServer(function (req, res) {
+    var urlParsed = url.parse(req.url, true);
+
+    console.log('Request: ', JSON.stringify(urlParsed));
+
+    res.writeHeader(200, { 'Content-Type': 'text/html;charset=utf-8' });
+
+    res.write('Request: ' + JSON.stringify(urlParsed));
+
+    res.end();
+}).listen('.nodejs.sock');
