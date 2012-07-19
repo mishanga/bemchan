5f8895d38eeae9fa0e50d5dc6594db577048b732
Benjamin Tambourine <vtambourine@yandex.ru>
2012-07-19 18:02:52 +0400

    i-server: draft

--- blocks/i-server/i-server.serv.js
+++ blocks/i-server/i-server.serv.js
@@ -1,6 +1,28 @@
-var express = require('express'),
-    app = express.createServer();
+var server = require('express').createServer();
 
-app.listen(3000);
+server.listen(3000);
 
-var serv = {};
+server.on('*', function(req, res) {
+
+    var ctx = {
+        req: req,
+        cgi: require('i-cgi'),
+        http: require('i-http')
+    };
+
+    bChan.serv(ctx, function(err) {
+        err && res.write('Error in serv');
+
+        bChan.priv(ctx, function(err) {
+            err && res.write('Error in priv');
+
+            bChan.bemhtml(ctx, function(err) {
+                err && res.write('Error in bemhtml');
+
+                res.write(ctx.html);
+            })
+        })
+    });
+
+});
+
